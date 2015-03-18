064c91a7cd74d4299a75884f65e12dd80d475f66
camunity <cameron.flowers13@gmail.com>
2015-03-18 16:59:51 -0500

    delegation working

--- RootViewController.m
+++ RootViewController.m
@@ -13,6 +13,9 @@
 @interface RootViewController ()<TopViewDelegate,HUDDelegate>
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *rightConstraint;
+@property (strong, nonatomic) IBOutlet UIView *hudContainerView;
+@property (strong, nonatomic) IBOutlet UIView *topContainerView;
+@property TopViewController *topVC;
 
 @end
 
@@ -20,12 +23,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    NSLog(@"class == %@", [self.childViewControllers[0] class]);
+    NSLog(@"class == %@", [self.childViewControllers[1] class]);
+
+    UINavigationController *temp = [self.childViewControllers objectAtIndex:1];
+    self.topVC = [temp.childViewControllers objectAtIndex:0];
+    NSLog(@"class == %@", [temp.childViewControllers[0] class]);
+    self.topVC.delegate = self;
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)topRevealButtonTapped:(id)viewController didTappedButton:(UIBarButtonItem *)sender{
+
+//    When the bar button item is tapped, drag IBOutlets from the left and right constraints on TopViewController. Change the constant property of the constraints so that the top view moves to the right (exposing HUDViewController and its two buttons). Implement this logic in your custom delegate method named topRevealButtonTapped.
+
+    
+    NSLog(@"%f,%f constraints",self.leftConstraint.constant, self.rightConstraint.constant);
+
 }
+
 
 @end
